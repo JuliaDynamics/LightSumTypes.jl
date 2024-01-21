74dfe35de927f868a2092fe3b4c2432cd1621957
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-21 02:40:18 +0100

    More tests (#5)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,3 @@
-
 using Test
 using MixedStructTypes
 
@@ -22,6 +21,29 @@ using MixedStructTypes
     end
 end
 
+@compact_struct_type Animal{T,N,J} begin
+    mutable struct Wolf{T,N}
+        energy::T = 0.5
+        ground_speed::N
+        const fur_color::Symbol
+    end
+    mutable struct Hawk{T,N,J}
+        energy::T = 0.1
+        ground_speed::N
+        flight_speed::J
+    end
+end
+
+abstract type AbstractSimple end
+@compact_struct_type Simple <: AbstractSimple begin
+    struct SimpleA
+        x::Int
+    end
+    struct SimpleB
+        y::Int
+    end
+end
+
 @testset "@sum_struct_type" begin
 
     b = B((1,1), (1.0, 1.0), :s)
@@ -34,6 +56,37 @@ end
     @test b.a == (3, 3)
 
     @test kindof(b) == :B
+
+    hawk_1 = Hawk(1.0, 2.0, 3)
+    hawk_2 = Hawk(; ground_speed = 2.3, flight_speed = 2)
+    wolf_1 = Wolf(2.0, 3.0, :black)
+    wolf_2 = Wolf(; ground_speed = 2.0, fur_color = :white)
+
+    @test hawk_1.energy == 1.0
+    @test hawk_2.energy == 0.1
+    @test wolf_1.energy == 2.0
+    @test wolf_2.energy == 0.5
+    @test hawk_1.flight_speed == 3
+    @test hawk_2.flight_speed == 2
+    @test wolf_1.fur_color == :black
+    @test wolf_2.fur_color == :white
+    @test_throws "" hawk_1.fur_color
+    @test_throws "" wolf_1.flight_speed
+    @test kindof(hawk_1) == kindof(hawk_2) == :Hawk
+    @test kindof(wolf_1) == kindof(wolf_2) == :Wolf 
+
+
+    b = SimpleA(1)
+    c = SimpleB(2)
+
+    @test b.x == 1
+    @test c.y == 2
+    @test_throws "" b.y
+    @test_throws "" c.x
+    @test kindof(b) == :SimpleA
+    @test kindof(c) == :SimpleB
+    @test Simple <: AbstractSimple
+    @test b isa Simple && c isa Simple  
 end
 
 @compact_struct_type E{X,Y} begin
@@ -56,6 +109,29 @@ end
     end
 end
 
+@compact_struct_type Animal2{T,N,J} begin
+    mutable struct Wolf2{T,N}
+        energy::T = 0.5
+        ground_speed::N
+        const fur_color::Symbol
+    end
+    mutable struct Hawk2{T,N,J}
+        energy::T = 0.1
+        ground_speed::N
+        flight_speed::J
+    end
+end
+
+abstract type AbstractSimple2 end
+@compact_struct_type Simple2 <: AbstractSimple2 begin
+    struct SimpleA2
+        x::Int
+    end
+    struct SimpleB2
+        y::Int
+    end
+end
+
 @testset "@compact_struct_type" begin
 
     f = F((1,1), (1.0, 1.0), :s)
@@ -68,4 +144,35 @@ end
     @test f.a == (3, 3)
 
     @test kindof(f) == :F
+
+    hawk_1 = Hawk2(1.0, 2.0, 3)
+    hawk_2 = Hawk2(; ground_speed = 2.3, flight_speed = 2)
+    wolf_1 = Wolf2(2.0, 3.0, :black)
+    wolf_2 = Wolf2(; ground_speed = 2.0, fur_color = :white)
+
+    @test hawk_1.energy == 1.0
+    @test hawk_2.energy == 0.1
+    @test wolf_1.energy == 2.0
+    @test wolf_2.energy == 0.5
+    @test hawk_1.flight_speed == 3
+    @test hawk_2.flight_speed == 2
+    @test wolf_1.fur_color == :black
+    @test wolf_2.fur_color == :white
+    @test_throws "" hawk_1.fur_color
+    @test_throws "" wolf_1.flight_speed
+    @test kindof(hawk_1) == kindof(hawk_2) == :Hawk2
+    @test kindof(wolf_1) == kindof(wolf_2) == :Wolf2 
+
+
+    b = SimpleA2(1)
+    c = SimpleB2(2)
+
+    @test b.x == 1
+    @test c.y == 2
+    @test_throws "" b.y
+    @test_throws "" c.x
+    @test kindof(b) == :SimpleA2
+    @test kindof(c) == :SimpleB2
+    @test Simple2 <: AbstractSimple2
+    @test b isa Simple2 && c isa Simple2   
 end
