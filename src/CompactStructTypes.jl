d2e5b486a1fd34919a2dcdc8d43293d895027c20
Tortar <68152031+Tortar@users.noreply.github.com>
2024-01-24 02:23:44 +0100

    Update CompactStructTypes.jl

--- src/CompactStructTypes.jl
+++ src/CompactStructTypes.jl
@@ -162,6 +162,7 @@ macro compact_struct_type(new_type, struct_defs = nothing)
             $(expr_comp_types...)
             $(Base.@__doc__ expr_new_type)
             $(expr_functions...)
+            export kindof
             $(expr_kindof)
             $(expr_getprop)
             $(expr_setprop)
