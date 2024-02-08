918c44558613c0fb8de0fe12de559e0f165b1f44
Tortar <68152031+Tortar@users.noreply.github.com>
2024-02-08 21:11:06 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 using MixedStructTypes
 
 @testset "MixedStructTypes.jl Tests" begin
-    include("test_sum_struct_type.jl")
-    include("test_compact_struct_type.jl")
+    include("test_sum_struct_type_macro.jl")
+    include("test_compact_struct_type_macro.jl")
 end
