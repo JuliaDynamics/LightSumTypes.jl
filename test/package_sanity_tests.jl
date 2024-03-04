bc824edd370a87d26961eb008c0a0df71f4d6d52
Tortar <68152031+Tortar@users.noreply.github.com>
2024-03-04 12:15:58 +0100

    Update package_sanity_tests.jl

--- test/package_sanity_tests.jl
+++ test/package_sanity_tests.jl
@@ -2,6 +2,6 @@
 using Aqua
 
 @testset "Code quality" begin
-	Aqua.test_all(MixedStructTypes, ambiguities = false, unbound_args = false)
+	Aqua.test_all(MixedStructTypes, ambiguities = false, unbound_args = true)
 	@test Test.detect_ambiguities(MixedStructTypes) == Tuple{Method, Method}[]
 end
