c441291b7d82655d67fe51dc99961f3cd04d72cf
Tortar <68152031+Tortar@users.noreply.github.com>
2024-03-11 20:27:07 +0100

    Update sum_structs_macro_tests.jl

--- test/sum_structs_macro_tests.jl
+++ test/sum_structs_macro_tests.jl
@@ -93,6 +93,7 @@ end
     @test allkinds(A) == (:B, :C, :D)
     @test allkinds(typeof(b)) == (:B, :C, :D)
     @test kindconstr(b) == B
+    @test kindconstr(d) == D
     
     hawk_1 = Hawk(1.0, 2.0, 3)
     hawk_2 = Hawk(; ground_speed = 2.3, flight_speed = 2)
