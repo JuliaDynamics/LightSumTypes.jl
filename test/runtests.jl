c81a1797cfdcb41ef5b700dd907703ecd93cb7cd
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-01 22:10:13 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 using Test
 using DynamicSumTypes
 
-@testset "MixedStructTypes.jl Tests" begin
+@testset "DynamicSumTypes.jl Tests" begin
     include("package_sanity_tests.jl")
     include("sum_structs_macro_tests.jl")
     include("compact_structs_macro_tests.jl")
