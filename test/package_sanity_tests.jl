e1af7e050259b42df5f8d9dd13860431787b6264
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-01 22:10:26 +0200

    Update package_sanity_tests.jl

--- test/package_sanity_tests.jl
+++ test/package_sanity_tests.jl
@@ -2,6 +2,6 @@
 using Aqua
 
 @testset "Code quality" begin
-    Aqua.test_all(MixedStructTypes, ambiguities = false, unbound_args = true)
-    @test Test.detect_ambiguities(MixedStructTypes) == Tuple{Method, Method}[]
+    Aqua.test_all(DynamicSumTypes, ambiguities = false, unbound_args = true)
+    @test Test.detect_ambiguities(DynamicSumTypes) == Tuple{Method, Method}[]
 end
