ad4e770fc7b82722427367e1afa548edfe1399e4
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-22 18:25:54 +0200

    Update SumStructsOnFields.jl

--- src/SumStructsOnFields.jl
+++ src/SumStructsOnFields.jl
@@ -258,7 +258,8 @@ function _compact_structs(new_type, struct_defs, vtc, vtwpc)
     expr_adjoint = :(Base.adjoint(::Type{<:$(namify(new_type))}) =
             $NamedTuple{$(Expr(:tuple, QuoteNode.(namify.(types_each_vis))...))}($(Expr(:tuple, namify.(types_each)...))))
 
-    fake_prints = [:(Base.show(io::IO, ::MIME"text/plain", T::Type{<:$(namify(fn))}) = print(io, $(string(namify(v))))) for (fn, v) in zip(types_each, types_each_vis)]
+    fake_prints = [:($Base.show(io::IO, ::MIME"text/plain", T::Type{<:$(namify(fn))}) = print(io, $(string(namify(new_type), "'.", namify(v))))) 
+                   for (fn, v) in zip(fake_names, variants_types_names)]
 
     expr_exports = def_export_variants(new_type)
 
