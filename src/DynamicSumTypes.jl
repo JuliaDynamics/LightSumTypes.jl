75951692cf7b70858b2cc42738979fd072d7cc6f
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-07-06 18:09:41 +0200

    Update DynamicSumTypes.jl

--- src/DynamicSumTypes.jl
+++ src/DynamicSumTypes.jl
@@ -5,6 +5,22 @@ export @sumtype
 
 unwrap(sumt) = getfield(sumt, :variants)
 
+"""
+    @sumtype SumTypeName(Types) [<: AbstractType]
+
+The macro creates a sumtypes composed by the given types.
+It optionally accept also an abstract supertype.
+
+## Example
+```julia
+julia> using DynamicSumTypes
+
+julia> struct A x::Int end;
+
+julia> struct B end;
+
+julia> @sumtype AB(A, B)
+"""
 macro sumtype(typedef)
 
     if typedef.head === :call
@@ -60,5 +76,49 @@ function branchs(variants, outputs)
     push!(branchs, :(error("THIS_SHOULD_BE_UNREACHABLE")))
     return branchs
 end
+
+"""
+    variant(inst)
+
+Returns the variant enclosed in the sum type.
+
+## Example
+```julia
+julia> using DynamicSumTypes
+
+julia> struct A x::Int end;
+
+julia> struct B end;
+
+julia> @sumtype AB(A, B)
+
+julia> a = AB(A(0))
+AB'.A(0)
+
+julia> variant(a)
+A(0)
+"""
+function variant end
+
+"""
+    allvariants(SumType)
+
+Returns all the enclosed variants types in the sum type
+in a tuple.
+  
+## Example
+```julia
+julia> using DynamicSumTypes
+
+julia> struct A x::Int end;
+
+julia> struct B end;
+
+julia> @sumtype AB(A, B)
+
+julia> allvariants(AB)
+(A, B)
+"""
+function allvariants end
 
 end
