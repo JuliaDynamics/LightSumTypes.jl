ee26bbdf03e2a81b1c8662dc272564871dc78f92
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-07-06 18:21:45 +0200

    Update sumtype_macro_tests.jl

--- test/sumtype_macro_tests.jl
+++ test/sumtype_macro_tests.jl
@@ -109,7 +109,7 @@ end
     b = Simple(SimpleA(1, 3))
     c = Simple(SimpleB(2, "a"))
 
-    @test (@capture_out begin print(b) end) == "Simple'.SimpleA(1, 3)"
+    @test (@capture_out begin show(b) end) == "\"Simple'.SimpleA(1, 3)\""
     @test b.x == 1 && b.z == 3
     @test c.y == 2 && c.q == "a"
     @test_throws "" b.y
