801daf9f05ec21b1e8d7c15bfe258469f059eea1
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-09-09 00:02:44 +0200

    Update precompile.jl

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,1 +1,19 @@
 
+using PrecompileTools
+
+@setup_workload begin
+    @compile_workload let
+        abstract type AbstractS end
+        struct A{X}
+            x::X
+        end
+        mutable struct B{Y}
+            y::Y
+        end
+        struct C
+            z::Int
+        end
+        _sumtype(:(S{X}(A{X},B{Int},C)))
+        _sumtype(:(S{X}(A{X},B{Int},C) <: AbstractS))
+    end
+end
