c6db408eb44b3cb80b6b6264edc5060fd809a6a6
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-09-09 16:32:18 +0200

    Improve precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -13,7 +13,8 @@ using PrecompileTools
         struct C
             z::Int
         end
-        _sumtype(:(S{X}(A{X},B{Int},C)))
+        _sumtype(:(S(C)))
+        _sumtype(:(S{X}(A{X},C)))
         _sumtype(:(S{X}(A{X},B{Int},C) <: AbstractS))
     end
 end
